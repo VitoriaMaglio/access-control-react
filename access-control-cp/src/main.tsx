5874e637a767dff19dde8c5b9176f6b84b4419f7
mateusgs1 <contatomateusgs@gmail.com>
2025-10-05 19:22:54 -0300

    Envolvendo  App com AuthProvider

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./routes/Login";
+import App from "./App";
+import { AuthProvider } from "./context";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
