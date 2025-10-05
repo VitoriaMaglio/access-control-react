0daf59da6d6db23c97b872d4515336f9b2bb3797
mateusgs1 <contatomateusgs@gmail.com>
2025-10-05 19:56:33 -0300

    Renderizando Header e mantendo rotas.

--- access-control-cp/src/App.tsx
+++ access-control-cp/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./routes/Login";
 import Cadastro from "./routes/Cadastro";
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
