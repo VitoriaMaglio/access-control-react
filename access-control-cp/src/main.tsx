3a389dbb518d77b9afd4f0826b75dd7af75412a7
FelipeMaglio <felipemagliofilho@gmail.com>
2025-10-05 12:22:14 -0300

    Criando base simples para página de Login, e adicionando na main.tsx.

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./routes/Login";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ access-control-cp/src/routes/Login.tsx
@@ -0,0 +1,29 @@
+const Login = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Página de Login</h1>
+
+      <form>
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="nomeUsuario">Nome de usuário:</label>
+          <br />
+          <input
+            id="nomeUsuario"
+            type="text"
+            placeholder="Digite seu nome de usuário"
+          />
+        </div>
+
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="email">Email:</label>
+          <br />
+          <input id="email" type="email" placeholder="Digite seu email" />
+        </div>
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
